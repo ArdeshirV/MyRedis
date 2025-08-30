cec699c90041906c4f969834822f805f489f4e84
ArdeshirV <e-job@protonmail.com>
2025-08-30 17:20:31 +0200

    Update

--- main.go
+++ main.go
@@ -7,3 +7,15 @@ import (
 func main() {
 	fmt.Println(MagentaBold, "The MyRedis Database!", Normal)
 }
+
+func Hello() {
+	fmt.Println("Hello")
+	fmt.Println("Goodbye")
+	AddTwoNumber(20, 30)
+}
+
+func AddTwoNumber(a, b int) int {
+	result := a + b
+	fmt.Println(result)
+	return result
+}
