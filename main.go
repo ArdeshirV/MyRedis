eba70c3da033b3087a91dd62870d98a9731bac29
ArdeshirV <e-job@protonmail.com>
2025-08-31 15:35:07 +0200

    Add NewDatabase() Set(key, value)

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+    "encoding/gob"
+    "os"
+    "sync"
 )
 
 func main() {
 	fmt.Println(MagentaBold, "The MyRedis Database!", Normal)
 }
+
+type Database struct {
+    data map[string]any
+    lock sync.RWMutex
+}
+
+func NewDatabase() *Database {
+    return &Database{
+        data: make(map[string]any)
+    }
+}
+
+func (db *Database) Set(key string, value any) {
+    db.lock.Lock()
+    defer db.lock.Unlock()
+    db.data[key] = value
+}
+
+
