765656a93594dbbb714db3f9e47c5000f7de6bb5
ArdeshirV <e-job@protonmail.com>
2025-08-31 07:08:44 +0200

    Update

--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	fmt.Println(MagentaBold, "The MyRedis Database!", Normal)
-	//Hello()
+	Hello()
 }
 
 func Hello() {
 	fmt.Println("Hello")
-	fmt.Println("Goodbye")
+	fmt.Println(ToUpper("Goodbye"))
+    fmt.Println(strings.ToUpper("Goodbye")
+}
+
+func ToUpper(text string) string {
+	const dist = 'a' - 'A'
+	var sb strings.Builder
+	for _, ch := range text {
+		if ch >= 'a' && ch <= 'z' {
+			sb.WriteRune(ch - dist)
+		} else {
+			sb.WriteRune(ch)
+		}
+	}
+	return sb.String()
 }
 
 func AddNumbers() {
